ec30f69b5925cc0bf8ea4d6352345d0f47ce35ff
catveloper365 <dkdmsl32@naver.com>
2023-07-07 04:21:45 +0900

    [#1]init project

---
+++ src/test/java/com/catveloper365/studyshop/StudyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catveloper365.studyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
