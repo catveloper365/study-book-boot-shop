3ab057b720953c20eec0a36a6a0def0a7ef6a506
catveloper365 <dkdmsl32@naver.com>
2023-09-25 06:57:45 +0900

    Test: 상품 리포지토리 - 저장,삭제,수정 테스트 [#4]

--- src/test/java/com/catveloper365/studyshop/repository/ItemRepositoryTest.java
+++ src/test/java/com/catveloper365/studyshop/repository/ItemRepositoryTest.java
@@ -2,7 +2,6 @@ package com.catveloper365.studyshop.repository;
 
 import com.catveloper365.studyshop.constant.ItemSellStatus;
 import com.catveloper365.studyshop.entity.Item;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +14,6 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -26,10 +24,12 @@ class ItemRepositoryTest {
     @PersistenceContext
     EntityManager em;
 
-    @Test
-    @DisplayName("저장 테스트")
-    public void save() {
-        //given
+    /**
+     * 테스트 데이터 생성
+     *
+     * @return Item
+     */
+    private Item createItem() {
         Item item = new Item();
         item.setItemNm("테스트 상품");
         item.setPrice(10000);
@@ -39,6 +39,14 @@ class ItemRepositoryTest {
         item.setRegTime(LocalDateTime.now());
         item.setUpdateTime(LocalDateTime.now());
         System.out.println(item);
+        return item;
+    }
+
+    @Test
+    @DisplayName("저장 테스트")
+    public void save() {
+        //given
+        Item item = createItem();
 
         //when
         itemRepository.save(item);
@@ -50,5 +58,43 @@ class ItemRepositoryTest {
         Optional<Item> findItem = itemRepository.findById(item.getId());
         assertThat(item.getItemNm()).isEqualTo(findItem.orElse(null).getItemNm());
         assertThat(item.getStockNumber()).isEqualTo(findItem.orElse(null).getStockNumber());
+    }
+
+    @Test
+    @DisplayName("삭제 테스트")
+    void delete() {
+        //given
+        Item item = createItem();
+        itemRepository.save(item);
+        long saveCnt = itemRepository.count();
+        assertThat(saveCnt).isEqualTo(1);
+
+        //when
+        itemRepository.delete(item);
+
+        //then
+        long removeCnt = itemRepository.count();
+        assertThat(removeCnt).isEqualTo(0);
+        Optional<Item> findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEmpty();
+    }
+
+    @Test
+    @DisplayName("수정 테스트")
+    void update() {
+        //given
+        Item item = createItem();
+        Item savedItem = itemRepository.save(item);
+
+        //when
+        savedItem.setPrice(15000);
+        savedItem.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+        em.flush();
+        em.clear();
+
+        //then
+        Optional<Item> findItem = itemRepository.findById(savedItem.getId());
+        assertThat(savedItem.getPrice()).isEqualTo(findItem.orElse(null).getPrice());
+        assertThat(savedItem.getItemSellStatus()).isEqualTo(findItem.orElse(null).getItemSellStatus());
     }
 }
