90be428c6d60369c84f2439f88c994b5df3d6c9a
catveloper365 <dkdmsl32@naver.com>
2023-09-26 04:10:30 +0900

    Test: 상품명or상세 설명 조회 메서드 테스트, 테스트 데이터 생성 메서드 추가 [#6]

--- src/test/java/com/catveloper365/studyshop/repository/ItemRepositoryTest.java
+++ src/test/java/com/catveloper365/studyshop/repository/ItemRepositoryTest.java
@@ -11,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,24 +27,34 @@ class ItemRepositoryTest {
 
     /**
      * 테스트 데이터 생성
-     *
-     * @return Item
      */
     private Item createItem() {
+        Item item = createItem("테스트 상품", 10000, "테스트 상품 상세 설명");
+        return item;
+    }
+
+    private Item createItem(String itemNm, int price, String itemDetail) {
         Item item = new Item();
-        item.setItemNm("테스트 상품");
-        item.setPrice(10000);
-        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemNm(itemNm);
+        item.setPrice(price);
+        item.setItemDetail(itemDetail);
         item.setItemSellStatus(ItemSellStatus.SELL);
         item.setStockNumber(100);
         item.setRegTime(LocalDateTime.now());
         item.setUpdateTime(LocalDateTime.now());
-        System.out.println(item);
+//        System.out.println(item);
         return item;
     }
 
+    private void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = createItem(("테스트 상품" + i), (10000 + i), ("테스트 상품 상세 설명" + i));
+            itemRepository.save(item);
+        }
+    }
+
     @Test
-    @DisplayName("저장 테스트")
+    @DisplayName("저장")
     public void save() {
         //given
         Item item = createItem();
@@ -61,7 +72,7 @@ class ItemRepositoryTest {
     }
 
     @Test
-    @DisplayName("삭제 테스트")
+    @DisplayName("삭제")
     void delete() {
         //given
         Item item = createItem();
@@ -82,7 +93,7 @@ class ItemRepositoryTest {
     }
 
     @Test
-    @DisplayName("수정 테스트")
+    @DisplayName("수정")
     void update() {
         //given
         Item item = createItem();
@@ -99,4 +110,62 @@ class ItemRepositoryTest {
         assertThat(savedItem.getPrice()).isEqualTo(findItem.orElseGet(Item::new).getPrice());
         assertThat(savedItem.getItemSellStatus()).isEqualTo(findItem.orElseGet(Item::new).getItemSellStatus());
     }
+
+    @Test
+    @DisplayName("상품명 조회")
+    void findByItemNmTest() {
+        //given
+        this.createItemList();
+
+        //when
+        String findItemNm = "테스트 상품5";
+        List<Item> itemList = itemRepository.findByItemNm(findItemNm);
+
+        //then
+//        for (Item item : itemList) {
+//            System.out.println("상품명 조회 결과 = " + item);
+//        }
+        assertThat(itemList.size()).isEqualTo(1);
+        assertThat(findItemNm).isEqualTo(itemList.get(0).getItemNm());
+    }
+
+    @Test
+    @DisplayName("상품명, 상품 상세 설명 모두 존재")
+    public void findNmOrDetailBoth() throws Exception {
+        //given
+        this.createItemList();
+
+        //when
+        String findItemNm = "테스트 상품10"; //DB에 있는 데이터
+        String findItemDetail = "테스트 상품 상세 설명5"; //DB에 있는 데이터
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail(findItemNm, findItemDetail);
+
+        //then
+//        for (Item item : itemList) {
+//            System.out.println("item = " + item);
+//        }
+        assertThat(itemList.size()).isEqualTo(2);
+        assertThat(findItemNm).isIn(itemList.get(0).getItemNm(), itemList.get(1).getItemNm());
+    }
+
+    @Test
+    @DisplayName("상품명, 상품 상세 설명 중 1개만 존재")
+    void findNmOrDetailOne() throws Exception {
+        //given
+        this.createItemList();
+
+        //when
+        String findItemNm = "테스트 상품"; //DB에 없는 데이터
+        String findItemDetail = "테스트 상품 상세 설명1"; //DB에 있는 데이터
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail(findItemNm, findItemDetail);
+
+        //then
+//        for (Item item : itemList) {
+//            System.out.println("item = " + item);
+//        }
+        assertThat(itemList.size()).isEqualTo(1);
+        assertThat(findItemDetail).isEqualTo(itemList.get(0).getItemDetail());
+    }
+
+
 }
