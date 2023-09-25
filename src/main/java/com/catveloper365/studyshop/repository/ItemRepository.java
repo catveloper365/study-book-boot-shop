69a566cddc76d81bf6201825f8618c285467de76
catveloper365 <dkdmsl32@naver.com>
2023-09-26 04:08:57 +0900

    Improve: 상품명or상세설명 조회 메서드 추가 [#6]

--- src/main/java/com/catveloper365/studyshop/repository/ItemRepository.java
+++ src/main/java/com/catveloper365/studyshop/repository/ItemRepository.java
@@ -3,5 +3,12 @@ package com.catveloper365.studyshop.repository;
 import com.catveloper365.studyshop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm); //상품명으로 조회
+
+    //상품명 혹은 상품 상세 설명으로 조회
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 }
