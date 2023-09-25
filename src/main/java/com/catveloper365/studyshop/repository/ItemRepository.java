e5495438f69872ccab9f22826b9fa271e4372c1c
catveloper365 <dkdmsl32@naver.com>
2023-09-26 05:02:11 +0900

    Improve: 가격 LessThan 메서드(오름차순,내림차순) 추가 [#6]

--- src/main/java/com/catveloper365/studyshop/repository/ItemRepository.java
+++ src/main/java/com/catveloper365/studyshop/repository/ItemRepository.java
@@ -11,4 +11,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     //상품명 혹은 상품 상세 설명으로 조회
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    //파라미터 값보다 가격이 작은 상품 조회(오름차순)
+    List<Item> findByPriceLessThan(Integer price);
+
+    //파라미터 값보다 가격이 작은 상품 조회(내림차순)
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
