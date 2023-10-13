7da0217b3c0e0dd73a9a22a8490f9508b4cfaa84
catveloper365 <dkdmsl32@naver.com>
2023-10-13 17:13:30 +0900

    Improve: findByEmail의 반환타입을 Optional로 변경 [#22]

--- src/main/java/com/catveloper365/studyshop/repository/MemberRepository.java
+++ src/main/java/com/catveloper365/studyshop/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.catveloper365.studyshop.repository;
 import com.catveloper365.studyshop.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
