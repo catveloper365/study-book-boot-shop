f595a0872ff4cb234afeaf3f5686041af69ed4fe
catveloper365 <dkdmsl32@naver.com>
2023-10-21 22:11:56 +0900

    Improve: MemberService에 UserDetailsService 인터페이스 구현 [#22]
    findByEmail 쿼리 메서드의 반환 타입을 Optional로 변경함에 따라 조회 결과를 처리하는 코드도 변경됨
    UserDetailsService 인터페이스의 loadUserByUsername 메서드 오버라이드 함, 사용자 이름으로 이메일을 사용

--- src/main/java/com/catveloper365/studyshop/service/MemberService.java
+++ src/main/java/com/catveloper365/studyshop/service/MemberService.java
@@ -3,6 +3,10 @@ package com.catveloper365.studyshop.service;
 import com.catveloper365.studyshop.entity.Member;
 import com.catveloper365.studyshop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,7 +14,7 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     public Member join(Member member) {
@@ -20,10 +24,21 @@ public class MemberService {
 
     //중복 회원 검사, 중복이면 예외 발생
     private void validateDuplicateMember(Member member) {
-        Member findMember = memberRepository.findByEmail(member.getEmail());
-        if (findMember != null) {
+        if (memberRepository.findByEmail(member.getEmail()).isPresent()) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member findMember = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+
+        //스프링 시큐리티에서 제공하는 UserDetails 구현체인 User 생성하여 반환
+        return User.builder()
+                .username(findMember.getEmail())
+                .password(findMember.getPassword())
+                .roles(findMember.getRole().toString())
+                .build();
     }
 }
