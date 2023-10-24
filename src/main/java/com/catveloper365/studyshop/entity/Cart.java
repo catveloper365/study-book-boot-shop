9dd8510e791e2bd6ebc2912776a7a2d3ef2f4f30
catveloper365 <dkdmsl32@naver.com>
2023-10-24 17:51:47 +0900

    New: 장바구니 엔티티 설계 및 연관관계 맵핑 [#26]
    장바구니 엔티티가 회원 엔티티를 참조하는 일대일 단방향 관계

---
+++ src/main/java/com/catveloper365/studyshop/entity/Cart.java
@@ -0,0 +1,24 @@
+package com.catveloper365.studyshop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@ToString
+public class Cart {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
