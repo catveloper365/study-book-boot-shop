0af3ac152b1e9f9ae201ec829f582ba1ccec66a8
catveloper365 <dkdmsl32@naver.com>
2024-03-23 09:15:13 +0900

    Improve: 상품 정보 조회, 수정 메서드 추가 [#36]

--- src/main/java/com/catveloper365/studyshop/service/ItemService.java
+++ src/main/java/com/catveloper365/studyshop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.catveloper365.studyshop.service;
 
 import com.catveloper365.studyshop.dto.ItemFormDto;
+import com.catveloper365.studyshop.dto.ItemImgDto;
 import com.catveloper365.studyshop.entity.Item;
 import com.catveloper365.studyshop.entity.ItemImg;
 import com.catveloper365.studyshop.repository.ItemImgRepository;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +24,7 @@ public class ItemService {
     private final ItemImgService itemImgService;
     private final ItemImgRepository itemImgRepository;
 
+    /** 상품 등록 */
     public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
         //상품 등록
         Item item = itemFormDto.createItem();
@@ -36,6 +40,42 @@ public class ItemService {
                 itemImg.setRepImgYn("N");
             }
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+
+    /** 특정 상품 정보 조회 */
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId) {
+        //해당 상품의 이미지 조회
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        //해당 상품의 상품 정보 조회
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+    }
+
+    /** 상품 수정 */
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+        //상품 수정
+        Item item = itemRepository.findById(itemFormDto.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        item.updateItem(itemFormDto);
+
+        List<Long> itemImgIds = itemFormDto.getItemImgIds();
+
+        //이미지 등록
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i));
         }
 
         return item.getId();
