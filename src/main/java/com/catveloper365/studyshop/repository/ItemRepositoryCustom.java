da6ab2c84a1e747b4a1f4852d04aecc27c685d17
catveloper365 <dkdmsl32@naver.com>
2024-03-25 04:34:42 +0900

    Improve: 사용자 정의 인터페이스에 메인 페이지 조회 메서드 추가 [#40]

--- src/main/java/com/catveloper365/studyshop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/catveloper365/studyshop/repository/ItemRepositoryCustom.java
@@ -1,10 +1,14 @@
 package com.catveloper365.studyshop.repository;
 
 import com.catveloper365.studyshop.dto.ItemSearchDto;
+import com.catveloper365.studyshop.dto.MainItemDto;
 import com.catveloper365.studyshop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ItemRepositoryCustom {
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
 }
