f757295b76965934b92355ba5c7614c44b4a3556
catveloper365 <dkdmsl32@naver.com>
2024-03-28 04:48:15 +0900

    New: 주문 이력 정보를 담을 DTO 클래스 생성 [#46]

---
+++ src/main/java/com/catveloper365/studyshop/dto/OrderHistDto.java
@@ -0,0 +1,28 @@
+package com.catveloper365.studyshop.dto;
+
+import com.catveloper365.studyshop.constant.OrderStatus;
+import com.catveloper365.studyshop.entity.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+public class OrderHistDto {
+    private Long orderId; //주문 아이디
+    private String orderDate; //주문 날짜
+    private OrderStatus orderStatus; //주문 상태
+    private List<OrderItemDto> orderItemDtoList = new ArrayList<>(); //주문 상품 리스트
+
+    public OrderHistDto(Order order) {
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.orderStatus = order.getOrderStatus();
+    }
+
+    public void addOrderItemDto(OrderItemDto orderItemDto) {
+        orderItemDtoList.add(orderItemDto);
+    }
+}
