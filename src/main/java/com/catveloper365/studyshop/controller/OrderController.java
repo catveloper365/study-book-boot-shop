2a7527015d8751bfa38e7c7183c6dc07d73c50bf
catveloper365 <dkdmsl32@naver.com>
2024-03-28 04:51:09 +0900

    Improve: 주문 이력 페이지 이동 메서드 추가 [#46]

--- src/main/java/com/catveloper365/studyshop/controller/OrderController.java
+++ src/main/java/com/catveloper365/studyshop/controller/OrderController.java
@@ -1,20 +1,24 @@
 package com.catveloper365.studyshop.controller;
 
 import com.catveloper365.studyshop.dto.OrderDto;
+import com.catveloper365.studyshop.dto.OrderHistDto;
 import com.catveloper365.studyshop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,6 +26,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    /** 주문 처리 요청 */
     @PostMapping(value = "/order")
     public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto,
                                               BindingResult bindingResult, Principal principal) {
@@ -43,5 +48,19 @@ public class OrderController {
         }
 
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+
+    /** 주문 이력 조회 페이지 이동 */
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> page,
+                            Principal principal, Model model) {
+        //pageSize 4
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
+
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+        model.addAttribute("orders", orderHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+        return "order/orderHist";
     }
 }
